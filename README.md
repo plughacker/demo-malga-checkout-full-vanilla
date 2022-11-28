416a097e092654c294615b638d255b1427d5a884
Filippe Rodrigues <filipperodrigues@rede.ulbra.br>
2022-11-28 14:28:04 -0300

    feat: Added PlugCheckoutFull valilla example

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# demo-plug-checkout-full-vanilla
+# Demo Plug Checkout Full VanillaJS
+
+This project is a demo of Plug Checkout Full running on VanillaJS
+
+## Running App
+
+Clone this app and open `index.html` in your browser
+
+## Learn More
+
+You can know more about Plug on our [website](https://www.plugpagamentos.com/).
---
+++ main.js
@@ -0,0 +1,80 @@
+(() => {
+  const plugCheckoutFull = document.querySelector("plug-checkout-full");
+
+  plugCheckoutFull.paymentMethods = {
+    pix: {
+      expiresIn: 3600,
+    },
+    credit: {
+      installments: {
+        quantity: 1,
+        show: true,
+      },
+      checkedSaveCard: false,
+      showCreditCard: true,
+    },
+    boleto: {
+      expiresDate: "2022-12-31",
+      instructions: "Instruções para pagamento do boleto",
+      interest: {
+        days: 1,
+        amount: 100,
+      },
+      fine: {
+        days: 2,
+        amount: 200,
+      },
+    },
+  };
+
+  plugCheckoutFull.transactionConfig = {
+    statementDescriptor: "#1 Demonstration Plug Checkout",
+    amount: 100,
+    description: "",
+    orderId: "",
+    customerId: "<CUSTOMER_ID>",
+    currency: "BRL",
+    capture: false,
+  };
+
+  plugCheckoutFull.dialogConfig = {
+    show: true,
+    actionButtonLabel: "Continuar",
+    errorActionButtonLabel: "Tentar novamente",
+    successActionButtonLabel: "Continuar",
+    successRedirectUrl: "https://www.plugpagamentos.com/",
+  };
+
+  plugCheckoutFull.pageConfig = {
+    brandUrl: "https://exampleurl/images/logo.png",
+    footerDescription: "Todos os direitos reservados © 2022 Plug Pagamentos.",
+    backRoute: "https://www.plugpagamentos.com/",
+    delivery: 0,
+    products: [
+      {
+        name: "Produto 1",
+        quantity: 2,
+        amount: 50,
+        description: "Descrição do produto 1",
+        sku: "123",
+        risk: "Low",
+      },
+      {
+        name: "Produto 2",
+        quantity: 2,
+        amount: 40,
+        description: "Descrição do produto 2",
+        sku: "124",
+        risk: "Low",
+      },
+    ],
+  };
+
+  plugCheckoutFull.addEventListener("paymentSuccess", ({ detail: { data } }) => {
+    console.log(data);
+  });
+
+  plugCheckoutFull.addEventListener("paymentError", ({ detail: { error } }) => {
+    console.log(error);
+  });
+})();
